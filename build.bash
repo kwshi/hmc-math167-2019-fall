e6e3adaf938f8254cda1c2f7f1b28a2d17cedace
Kye W. Shi <kwshi@hmc.edu>
2019-10-01 14:15:40 -0700

    remember to commit generated pdfs

--- build.bash
+++ build.bash
@@ -7,6 +7,7 @@ do tectonic "$f"
 done
 
 git add --force */*.pdf
+git commit -m 'generate pdfs'
 git \
   -c http.extraHeader="Authorization: basic $(printf '%s:%s' kwshi "$GITHUB_TOKEN" | base64)" \
   push origin pdfs
